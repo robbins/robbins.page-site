5cca688e64a1a217c3f795a445cc11d103239a88
Nathanael Robbins <nejrobbins@gmail.com>
2023-01-01 17:35:20 -0500

    Add website package & Zola theme

--- flake.nix
+++ flake.nix
@@ -1,14 +1,29 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    hermit-zola = { url = "github:VersBinarii/hermit_zola"; flake = false; };
   };
   
-  outputs = { self, nixpkgs }: {
-    packages.x86_64-linux.default = with import nixpkgs { system = "x86_64-linux"; };
-    stdenv.mkDerivation {
-      pname = "robbins-cc-zola";
-      src = ./robbins-cc-src;
-      buildInputs = [ zola ];
+  outputs = { self, nixpkgs, hermit-zola}:
+    let
+      themeName = "hermit";
+    in
+    {
+      packages.x86_64-linux.default = with import nixpkgs { system = "x86_64-linux"; };
+      stdenv.mkDerivation {
+        name = "robbins-cc-zola";
+        src = ./.;
+        buildInputs = with pkgs; [ zola ];
+        configurePhase = ''
+          mkdir -p "themes/${themeName}"
+          cp -r ${hermit-zola}/* "themes/${themeName}"
+        '';
+        buildPhase = ''
+          zola build
+        '';
+        installPhase = ''
+          cp -r public $out
+        '';
+      };
     };
-  };
 }
